749345a82844d2b89ad11721ca4428d873fb2b38
humdings <mail.dedwards@gmail.com>
2018-02-22 16:37:54 -0800

    Make account values work for non base currencies also.

--- crypto_hub/gdax/gdax_client.py
+++ crypto_hub/gdax/gdax_client.py
@@ -23,8 +23,21 @@ class GDAXClient(object):
         self.public_client = socket_client.public_client
 
     def account_values_in_base_currency(self, base_currency='BTC'):
-        accounts = self.auth_client.get_accounts()
+        """
+        Returns a series of account values in the base currency.
+
+        :param base_currency:
+        :return: pd.Series
+        """
         quotes = self.socket_client.get_bids()
+
+        if base_currency not in ('BTC', 'USD'):
+            usd_values = self.account_values_in_base_currency(base_currency='USD')
+            usd_cross = '-'.join([base_currency, 'USD'])
+            cross_rate = quotes.loc[usd_cross]
+            return usd_values / cross_rate
+
+        accounts = self.auth_client.get_accounts()
         values = pd.Series(0.0, index=accounts.index)
         for currency, row in accounts.iterrows():
             amount = row.balance
@@ -32,8 +45,12 @@ class GDAXClient(object):
                 values[base_currency] += amount
                 continue
             product_id = '-'.join([currency, base_currency])
-            if product_id not in quotes.index:
-                continue
-            exchange_rate = quotes.loc[product_id]
-            values[currency] = amount * exchange_rate
+            other_id = '-'.join([base_currency, currency])
+            if product_id in quotes.index:
+                exchange_rate = quotes.loc[product_id]
+                values[currency] += amount * exchange_rate
+            if other_id in quotes.index:
+                # Only BTC should trigger this.
+                exchange_rate = quotes.loc[other_id]
+                values[currency] += amount / exchange_rate
         return pd.Series(values)
